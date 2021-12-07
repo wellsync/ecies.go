95a9d816562dca607bb58daf5ab9817b22bb1d71
Dillen Meijboom <info@excol.nl>
2021-12-07 14:56:35 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nuts-foundation/crypto-ecies
+
+go 1.17
---
+++ update.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+VERSION="1.10.13"
+
+if ! [ -d "/tmp/go-eth" ]; then
+  git clone git@github.com:ethereum/go-ethereum.git /tmp/go-eth
+fi
+
+(cd /tmp/go-eth && git fetch --tags && git checkout v${VERSION}) && \
+    cp /tmp/go-eth/crypto/ecies/* ./
